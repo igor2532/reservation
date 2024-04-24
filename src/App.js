22bd2059cfd2cc874255f5c68d3313452494a9b9
Игорь Щука <admin1c@lidkon.local>
2024-04-24 17:00:26 +0300

    change day

--- src/App.js
+++ src/App.js
@@ -1,52 +1,83 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Modal from './Modal';
 
 function App() {
-  const month = [
-    {title:1,dayOfWeek:'Пн',isWeekend:false},
-    {title:10,dayOfWeek:'Вт',isWeekend:false},
-    {title:11,dayOfWeek:'Ср',isWeekend:false},
-    {title:12,dayOfWeek:'Чт',isWeekend:false},
-    {title:13,dayOfWeek:'Пт',isWeekend:false},
-    {title:14,dayOfWeek:'Сб',isWeekend:true},
-    {title:15,dayOfWeek:'Вс',isWeekend:true},
-    {title:16,dayOfWeek:'Пн',isWeekend:false},
-    {title:17,dayOfWeek:'Вт',isWeekend:false},
-    {title:18,dayOfWeek:'Ср',isWeekend:false},
-    {title:19,dayOfWeek:'Чт',isWeekend:false},
-    {title:20,dayOfWeek:'Пт',isWeekend:false},
-    {title:21,dayOfWeek:'Сб',isWeekend:true},
-    {title:22,dayOfWeek:'Вс',isWeekend:true},
-    {title:23,dayOfWeek:'Пн',isWeekend:false},
-    {title:24,dayOfWeek:'Вт',isWeekend:false},
-    {title:25,dayOfWeek:'Ср',isWeekend:false},
-    {title:26,dayOfWeek:'Чт',isWeekend:false},
-    {title:27,dayOfWeek:'Пт',isWeekend:false},
-    {title:28,dayOfWeek:'Сб',isWeekend:true},
-    {title:29,dayOfWeek:'Вс',isWeekend:true},
-    {title:30,dayOfWeek:'Пн',isWeekend:false},
-  ]
+ 
+ const [arrayValue, setArrayValue] = useState([]);
+ const [isModal, setIsModal] = useState(false)
 
 
-  fetch('https://myapi-5b0f.onrender.com/')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+ 
+ const sasa = () => {
+
+console.log(arrayValue)
+ }
+ 
+
+ async function getPosts() {
+  const res = await fetch('http://localhost:3001/',{ referrer:'unsafe-url'})
+    .then(response => response.json())
+    .then(data =>  setArrayValue(Object.values(data)))
+    .catch(error => console.error(error))
+  
+}
+
+
+// async function fetchData() {
+//   await axios("http://localhost:3001/").catch(()=>{
+//     console.log('Нет соединения')
+//   }).then((data)=>{ 
+    
+//     if (data == undefined) {
+//       setArrayValue(Object.values(data))
+//     }
+//    });
+  
+  // axios.get('http://localhost:3001/')
+  // .catch(function (error) {
+   
+  //   if (error.response) {
+  //     console.log(error.response.data);
+  //     console.log(error.response.status);
+  //     console.log(error.response.headers);
+  //   } else if (error.request) {
+   
+  //     console.log(error.request);
+  //   } else {
+  //     console.log('Error', error.message);
+  //   }
+  //   console.log(error.config);
+  // }).then((data)=>{
+  //   setArrayValue(Object.values(data))
+  // });
+
+
+
+ 
+// }
+
+useEffect(() => {
+  getPosts();
+}, [arrayValue]);
 
   return (
-    <div className="App">
+<>
+<div className="App" style={{filter:isModal?'blur(4px)':'unset'}}>
+     <div className='saH'>
+    
+</div>
+      <div onClick={()=>isModal?setIsModal(false):''} className='App_calendar_month'>
      
-
-
-
-   
-      <div className='App_calendar_month'>
-     
-    {month.map( (item,key) => (
-      <div>
-        <div style={{color:item.isWeekend?'red':'black',fontWeight:'bold'}} className='App_calendar_dayOf'>{item.dayOfWeek}</div>
-       <div className='App_calendar_day'>{item.title}</div> 
-       <div><button>Заказать билет</button></div>
+    {arrayValue.length>0 &&
+    arrayValue.map( (item,key) => (
+      <div style={{background:item.freeTickets==0?'#f8c3c3':'#cdf1cd'}}>
+        <div style={{color:item.isWeekend?'red':'black',fontWeight:'bold'}} className='App_calendar_dayOf'>{item.weekOfDay}</div>
+       <div className='App_calendar_day'>{item.id}</div> 
+       
+       <div className='App_calendar_button'><button  onClick={()=>setIsModal(!isModal)} style={{background:item.freeTickets==0?'red':'green'}}>({item.freeTickets})</button></div>
       </div>
     ))}
 
@@ -57,6 +88,10 @@ function App() {
 
 
     </div>
+      {isModal &&
+        <Modal />
+        }
+        </>
   );
 }
 
---
+++ src/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Modal() {
+  return (
+    <>
+ 
+  <div className='App_modal'>
+    <div className='App_modal_form'>Заказывайте билеты</div>
+  </div>
+  </>
+  )
+}
