15c56c0a1126ac99ef6f2e1dc6da4741759c97dd
unknown <igorvitalevich1994@gmail.com>
2024-04-24 20:27:57 +0300

    add modal

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
  
  const [arrayValue, setArrayValue] = useState([]);
  const [isModal, setIsModal] = useState(false)
-
+const [monthValue, setMonthValue] = useState('Апрель')
 
  
  const sasa = () => {
@@ -26,58 +26,26 @@ console.log(arrayValue)
 }
 
 
-// async function fetchData() {
-//   await axios("http://localhost:3001/").catch(()=>{
-//     console.log('Нет соединения')
-//   }).then((data)=>{ 
-    
-//     if (data == undefined) {
-//       setArrayValue(Object.values(data))
-//     }
-//    });
-  
-  // axios.get('http://localhost:3001/')
-  // .catch(function (error) {
-   
-  //   if (error.response) {
-  //     console.log(error.response.data);
-  //     console.log(error.response.status);
-  //     console.log(error.response.headers);
-  //   } else if (error.request) {
-   
-  //     console.log(error.request);
-  //   } else {
-  //     console.log('Error', error.message);
-  //   }
-  //   console.log(error.config);
-  // }).then((data)=>{
-  //   setArrayValue(Object.values(data))
-  // });
-
-
-
- 
-// }
 
 useEffect(() => {
   getPosts();
-}, [arrayValue]);
+}, [arrayValue,monthValue]);
 
   return (
 <>
-<div className="App" style={{filter:isModal?'blur(4px)':'unset'}}>
-     <div className='saH'>
-    
+<div onClick={()=>isModal?setIsModal(false):''} className="App" style={{filter:isModal?'blur(4px)':'unset'}}>
+     <div className='App_top'>
+    <h1>{monthValue}</h1>
 </div>
-      <div onClick={()=>isModal?setIsModal(false):''} className='App_calendar_month'>
+      <div  className='App_calendar_month'>
      
     {arrayValue.length>0 &&
     arrayValue.map( (item,key) => (
-      <div style={{background:item.freeTickets==0?'#f8c3c3':'#cdf1cd'}}>
+      <div style={{background:item.freeTickets==0?'#f8c3c3':'#cdf1cd',borderColor:item.freeTickets==0?'white':'#005d58'}}>
         <div style={{color:item.isWeekend?'red':'black',fontWeight:'bold'}} className='App_calendar_dayOf'>{item.weekOfDay}</div>
        <div className='App_calendar_day'>{item.id}</div> 
        
-       <div className='App_calendar_button'><button  onClick={()=>setIsModal(!isModal)} style={{background:item.freeTickets==0?'red':'green'}}>({item.freeTickets})</button></div>
+       <div className='App_calendar_button'><button disabled={item.freeTickets==0}  onClick={()=>setIsModal(!isModal)} style={{display:item.freeTickets==0?'none':''}}>({item.freeTickets})</button></div>
       </div>
     ))}
 
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -5,7 +5,32 @@ export default function Modal() {
     <>
  
   <div className='App_modal'>
-    <div className='App_modal_form'>Заказывайте билеты</div>
+    <div className='App_modal_form'>
+      <div className='App_modal_top'>
+        <h2>Четверг 28 апреля 2024г</h2>
+      </div>
+      <div className='App_modal_bottom'>
+       <div className='App_Modal_TimeBlocks'>
+        <div>
+          <span className='App_modal_time'>14:00</span>
+          <span className='App_modal_tickets'>15</span>
+        </div>
+        <div>
+          <span className='App_modal_time'>15:00</span>
+          <span className='App_modal_tickets'>15</span>
+        </div>
+        <div>
+          <span className='App_modal_time'>16:00</span>
+          <span className='App_modal_tickets'>15</span>
+        </div>
+        <div>
+          <span className='App_modal_time'>17:00</span>
+          <span className='App_modal_tickets'>15</span>
+        </div>
+       </div>
+
+      </div>
+    </div>
   </div>
   </>
   )
